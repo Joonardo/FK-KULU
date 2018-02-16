a22bdab26bb1352a86eb06ee92cbb541919f8390
Joonardo <joonardob@gmail.com>
2018-02-16 19:34:37 +0200

    Login works.

--- src/Security/token.py
+++ src/Security/token.py
@@ -6,17 +6,16 @@ from Security.password import verify
 from App import app
 from DB import User
 
-ser = TimedJSONWebSignatureSerializer(app.config['SECRET_KEY'], expires_in=3600)
+ser = TimedJSONWebSignatureSerializer(app.config['SECRET'], expires_in=3600)
 
 def auth(**kw):
     try:
-        data = ser.loads(kw['data']['token'].decode('ascii'))
+        data = ser.loads(request.headers['auth'].encode('ascii'))
     except BadSignature:
         raise ProcessingException(description='Oops, something went wrong with your token...')
     except SignatureExpired:
         raise ProcessingException(description='Your token has expired.', code=400)
 
-    del kw['data']['token'] # Is this necessary?
     g.user = User.query.filter_by(username=data['username'])
 
 def get_auth_token(username, password):
@@ -27,7 +26,7 @@ def get_auth_token(username, password):
 
     if verify(password, user.password_hash):
         print(user.username)
-        return ['Success', ser.dumps({'username': user.username})]
+        return ['Success', ser.dumps({'username': user.username}).decode('ascii')]
 
     return ['Wrong password or username.', '']
 

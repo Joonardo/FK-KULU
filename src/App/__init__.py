34f05cac0b0328b0c88b5cb2ad305647297e4678
Joonardo <joonardob@gmail.com>
2018-04-21 01:50:31 +0300

    Added missing files.

--- src/App/__init__.py
+++ src/App/__init__.py
@@ -5,11 +5,11 @@ app.config.from_object('dev_config')
 
 if not app.debug:
     import logging
-    fh = logging.FileHandler(app.config["LOG_FILE"])
+    fh = logging.FileHandler("./log")
     fh.setFormatter(logging.Formatter(app.config["LOG_FORMAT"]))
     fh.setLevel(logging.WARNING)
     app.logger.addHandler(fh)
-    logging.getLogger('sqlalchemy').addHandler(fh)
+    logging.getLogger('flask-sqlalchemy').addHandler(fh)
 
 from App import routes
 import DB
